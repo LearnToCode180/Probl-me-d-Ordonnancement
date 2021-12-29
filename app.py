999ae46b55df28039370282956d79f2f17d74fba
A Programmer <learntocode180@gmail.com>
2021-12-29 19:36:47 +0100

    fixing some errors

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, Blueprint
 from views import views
 
-scheduling_app = Flask(__name__)
+
 
 if __name__ == '__main__':  
-    
-    scheduling_app.register_blueprint(views, url_prefix='/')
+    app = Flask(__name__)
+    app.register_blueprint(views, url_prefix='/')
 
-    scheduling_app.run(debug=True)
+    app.run(debug=True)
